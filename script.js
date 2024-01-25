8fcae6fc58a449bfd6dd4b10366c408d9372e395
0kan1 <test467876@gmail.com>
2024-01-25 19:02:12 +0300

    main

---
+++ script.js
@@ -0,0 +1,17 @@
+function getRandomQuote() {
+    var txtDosyaYolu = 'sozler.txt';
+
+    var xhr = new XMLHttpRequest();
+    xhr.onreadystatechange = function() {
+        if (xhr.readyState === 4 && xhr.status === 200) {
+            var sozler = xhr.responseText.split('\n');
+            var randomIndex = Math.floor(Math.random() * sozler.length);
+            var randomSoz = sozler[randomIndex];
+
+            document.getElementById('soz').innerText = randomSoz;
+        }
+    };
+    xhr.open('GET', txtDosyaYolu, true);
+    xhr.send();
+}
+document.addEventListener('DOMContentLoaded', getRandomQuote);
