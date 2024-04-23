072c867cd9937b3782b3b60476c62e2e0e5ede49
0kan1 <test467876@gmail.com>
2024-04-23 15:21:33 +0300

    main

---
+++ sites/4IAJHTM0IdVz6g6YY2Bo52/chunk-FHKZDMBQ.mjs
@@ -0,0 +1,2 @@
+function t(a,e){return{description:"hello",favicon:"https://framerusercontent.com/assets/Lk1TVZSv1mI4OidcCDZtNvhDk.png",socialImage:"https://framerusercontent.com/assets/Lk1TVZSv1mI4OidcCDZtNvhDk.png",title:"Creative Studios"}}export{t as a};
+//# sourceMappingURL=chunk-FHKZDMBQ.mjs.map
